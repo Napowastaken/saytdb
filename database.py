d28b1d9e3d43bd2f0297ba9864ebbf452c4c3710
Napowastaken <santinocatuara@gmail.com>
2022-05-27 20:06:36 -0300

    Improved typehints

--- database.py
+++ database.py
@@ -1,10 +1,16 @@
 import json;
+from typing import *
 class create():
     def __init__(self, path):
         self.path = path
         self.load()
 
-    def save(self):
+    def save(self) -> NoReturn:
+        """ Saves the database's content to the file.
+
+        .. note::
+            
+            The module already saves for you, so you won't need this unless it's a specific case."""
         readable = open(self.path)
         restorable = json.load(readable)
         file = open(self.path, 'w')
@@ -19,12 +25,27 @@ class create():
         file.close()
         readable.close()
     
-    def load(self):
+    def load(self) -> NoReturn:
+        """ Loads the database content.
+        
+        .. note::
+            The module already loads for you, so you won't need this unless it's a specific case. However, it might be useful if you have multiple files with the same database."""
         file = open(self.path)
         self.content = json.load(file)
         file.close()
 
-    def set(self, key: str, value):
+    def set(self, key: str, value: any) -> NoReturn:
+        """Set a key to a specific value in the database or create one if it doesn't exist.
+        
+        Parameters
+        -----------
+        key: :class:`str`
+            The name of the key to set.
+
+        value: :class:`any`
+            The value of the key.
+        -----------
+        """
         if not isinstance(key, str):
             print(f'Key name type must be str, got {type(key)}')
             return
@@ -32,7 +53,18 @@ class create():
         self.content[key] = value
         self.save()
     
-    def append(self, key: str, value):
+    def append(self, key: str, value: any) -> NoReturn:
+        """Append an item to a list or create one if it doesn't exist or it's from a different type.
+
+        Parameters
+        -----------
+        key: :class:`str`
+            The name of the key to append.
+
+        value: :class:`any`
+            The item of the key.
+        -----------
+        """
         if not isinstance(key, str):
             print(f'Key name type must be str, got {type(key)}')
             return
@@ -45,7 +77,18 @@ class create():
         except BaseException as err:
             print(err)
 
-    def pop(self, key: str, index: int):
+    def pop(self, key: str, index: int) -> NoReturn:
+        """Pop an item in a list.
+
+        Parameters
+        -----------
+        key: :class:`str`
+            The name of the key to pop.
+
+        index: :class:`int`
+            Index of the item to remove.
+        -----------
+        """
         if not isinstance(key, str):
             print(f'Key name type must be str, got {type(key)}')
             return
@@ -58,17 +101,41 @@ class create():
         self.content[key].pop(index)
         self.save()
     
-    def get(self, key):
+    def get(self, key: str) -> any:
+        """Return the key's value or None if it doesn't exist.
+
+        Parameters
+        -----------
+        key: :class:`str`
+            The name of the key to get.
+        -----------
+        """
         self.load()
         try:
             return self.content[key]
         except:
             return None
     
-    def has(self, key):
+    def has(self, key: str) -> bool:
+        """Returns a boolean checking whether a key exists or not in the database.
+
+        Parameters
+        -----------
+        key: :class:`str`
+            The name of the key to check.
+        -----------
+        """
         return self.get(key) is not None
     
-    def delete(self, key: str):
+    def delete(self, key: str) -> NoReturn:
+        """Delete a key from the database.
+
+        Parameters
+        -----------
+        key: :class:`str`
+            The name of the key to delete.
+        -----------
+        """
         if not isinstance(key, str):
             print(f'Key name type must be str, got {type(key)}')
             return
@@ -79,6 +146,11 @@ class create():
         except:
             pass
     
-    def clear(self):
+    def clear(self) -> NoReturn:
+        """Clears all the database.
+
+        .. warning::
+            This will remove everything from the database and this action is irreversible, so you must use it carefully.
+        """
         self.content = {}
         self.save()
